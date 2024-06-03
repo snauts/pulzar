14fe21c4ee90dd7ef61b6e499f2f475fd13d4cfa
Artūrs Grebstelis <arturs@mikrotik.com>
2024-06-03 15:45:22 +0300

    move solar pattern in level list

--- main.c
+++ main.c
@@ -690,8 +690,8 @@ static void emit_radiate(void) {
 static const struct Level level_list[] = {
     { &emit_whirler, "WHIRLER" },
     { &emit_reverse, "REVERSE" },
-    { &emit_solaris, "*SOLAR*" },
     { &emit_squigle, "WAVEING" },
+    { &emit_solaris, "*SOLAR*" },
     { &emit_radiate, "RADIATE" },
     { &emit_curve,   "&CURVES" },
     { &emit_diamond, "DIAMOND" },
